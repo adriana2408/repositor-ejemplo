cd1775187900db44dacbbb7df10aeb4d99e2a16a
adriana2408 <adriana_trujilloneyra@outlook.com>
2024-10-02 18:44:02 -0500

    holi

---
+++ assets/js/firabase.js
@@ -0,0 +1,21 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAsMI20EsVz0ITlctiY8IrYYPKr8SLmUVM",
+  authDomain: "diario-digital-1f661.firebaseapp.com",
+  projectId: "diario-digital-1f661",
+  storageBucket: "diario-digital-1f661.appspot.com",
+  messagingSenderId: "268342626744",
+  appId: "1:268342626744:web:7a5dce303b55edcc35e74d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//Exportamos la app
+export { app };
---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+//Importamos la app
+import { app } from "./firabase.js";
+
+console.log(app)
